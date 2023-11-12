ead45bbf25b4f3752a4b09c3c43874b94f2d1633
Jeremy Rapich <jeremysr@protonmail.com>
2023-11-12 15:37:35 -0700

    added typeDefs based on logic happening in the RESTful controllers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+const typeDefs = `
+    type Book {
+        _id: ID!
+        authors: String
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+        savedBooks: [Book]
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        user(_id: String, username: String): User 
+    }
+
+    type Mutation {
+        createUser(username: String!, email: String!, password: String!): Auth
+        login(username: String!, password: String!): Auth
+        saveBook(
+            authors: String!,
+            description: String!,
+            bookId: String!,
+            image: String!,
+            link: String!,
+            title: String!
+        ): User
+        deleteBook(bookId: String!): User
+    }
+`;
+
+module.exports = typeDefs;
